dac6eed0aca75f244d3408764b07869b096aaa0c
lergns <lergns@gmail.com>
2021-05-19 16:35:10 +0300

    7 hws

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import HW7 from "../../h7/HW7";
 
 function Junior() {
-  return <div>Junior</div>;
+  return (
+    <div>
+      <HW7 />
+    </div>
+  );
 }
 
 export default Junior;
